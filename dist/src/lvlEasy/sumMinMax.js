10a84de11cabe44dc568eb9babbffaeb0a678bf9
Luis Carrion <lcarrionalvarez@gmail.com>
2022-01-31 17:56:53 -0500

    Exorcise updated

--- dist/src/lvlEasy/sumMinMax.js
+++ dist/src/lvlEasy/sumMinMax.js
@@ -1,18 +1,27 @@
 "use strict";
 function miniMaxSum(arr) {
-    var aux;
-    var i;
-    var j;
-    var n = arr.length;
+    /* let aux, i, j:number;
+    let n:number = arr.length;
     for (i = 1; i < n; i++) {
-        for (j = 0; j < (n - i); j++) {
-            if (arr[j] > arr[j + 1]) {
-                aux = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = aux;
-            }
+      for (j = 0; j < (n-i); j++) {
+        if (arr[j] > arr[j+1]) {
+          aux = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = aux;
+        }
+      }
+    }
+    console.log(arr); */
+    var menor = arr[0];
+    var mayor = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i + 1] < menor) {
+            menor = arr[i + 1];
+        }
+        else if (arr[i + 1] > mayor) {
+            mayor = arr[i + 1];
         }
     }
-    console.log(arr);
+    console.log(menor, mayor);
 }
 miniMaxSum([-5, 10, 3, 5, 8, 1, 9]);
--- src/lvlEasy/sumMinMax.ts
+++ src/lvlEasy/sumMinMax.ts
@@ -1,10 +1,7 @@
 
 function miniMaxSum(arr: number[]): void {  
-  let aux:number;
-  let i:number;
-  let j:number;
+  /* let aux, i, j:number;
   let n:number = arr.length;
-
   for (i = 1; i < n; i++) {
     for (j = 0; j < (n-i); j++) {
       if (arr[j] > arr[j+1]) {
@@ -14,7 +11,17 @@ function miniMaxSum(arr: number[]): void {
       }
     }
   }
-  console.log(arr);
+  console.log(arr); */
+  let menor:number=arr[0];
+  let mayor:number=arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i+1]<menor) {
+      menor = arr[i+1]
+    } else if (arr[i+1]>mayor) {
+      mayor = arr[i+1]
+    }
+  }
+  console.log(menor, mayor);
 }
 miniMaxSum([-5 ,10, 3, 5, 8, 1, 9]);
 
